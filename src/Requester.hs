a9ed274b334c76a4453599146f1be7ab4a0c51d6
Igor Belousov <lipranu@gmail.com>
2020-08-30 22:12:13 +0700

    Added a type for the result of queries and decoding, added functions for interaction with MonadRequester

--- src/Requester.hs
+++ src/Requester.hs
@@ -1,29 +1,31 @@
---{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ExplicitForAll #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ConstraintKinds #-}
 
 module Requester
   ( MonadRequester (..)
   , Requester
 
+  , decode
   , mkRequester
+  , request
+  , requestAndDecode
   ) where
 
 -- IMPORTS ---------------------------------------------------------------------
 
+import Internal                     ( Has (..) )
 
---import qualified Data.Aeson as Aeson
+import Data.Aeson                   ( FromJSON, eitherDecode )
 import Control.Monad.Reader         ( MonadReader, asks )
-import Data.Text (Text)
-import Network.HTTP.Client.Extended ( Request
-                                    , Response
-                                    , Manager
-                                    , HttpException
-                                    )
+import Data.Text                    (Text)
+import Data.Text.Encoding           (decodeUtf8)
+import Network.HTTP.Client.Extended ( Request, Response
+                                    , Manager, HttpException )
 
-import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Lazy         as BSL
 import qualified Network.HTTP.Client.Extended as HTTP
-import qualified Data.Text as Text
+import qualified Data.Text                    as Text
 
 -- CLASSES ---------------------------------------------------------------------
 
@@ -37,12 +39,41 @@ class ToRequest a where
 
 -- TYPES AND INSTANCES ---------------------------------------------------------
 
+type HasRequester r m = (Has (Requester m) r, MonadReader r m, MonadRequester m)
+
 newtype Requester m = Requester
   { unRequester :: Request
                 -> m (Either HttpException (Response BSL.ByteString))
   }
 
+data Result a
+  = Result a
+  | DecodeError Text Text
+  | RequestError Text
+
 -- FUNCTIONS -------------------------------------------------------------------
 
 mkRequester :: MonadRequester m => Manager -> Requester m
 mkRequester = Requester . requester
+
+request :: (ToRequest a, HasRequester r m)
+        => a
+        -> m (Result BSL.ByteString)
+request r = do
+  req    <- asks getter
+  result <- unRequester req $ toRequest r
+  case result of
+    Left  e -> return $ RequestError $ Text.pack $ show e
+    Right r -> return $ Result $ HTTP.responseBody r
+
+decode :: forall a . FromJSON a => Result BSL.ByteString -> Result a
+decode (Result bs)        = case eitherDecode bs of
+  Left  e -> DecodeError (Text.pack e) $ decodeUtf8 $ BSL.toStrict bs
+  Right r -> Result r
+decode (RequestError e)   = RequestError e
+decode (DecodeError  e t) = DecodeError  e t
+
+requestAndDecode :: forall b a r m . (ToRequest a, FromJSON b, HasRequester r m)
+                 => a
+                 -> m (Result b)
+requestAndDecode req = decode <$> request req
