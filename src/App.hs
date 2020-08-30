7dd61e3843d04f86368447763d8e09b1fc64623e
Igor Belousov <lipranu@gmail.com>
2020-08-31 00:20:53 +0700

    Simplified parsing of top-level configuration

--- src/App.hs
+++ src/App.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE OverloadedStrings           #-}
-{-# LANGUAGE RecordWildCards           #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE DeriveGeneric     #-}
 
 module App ( run ) where
 
@@ -10,29 +11,26 @@ import qualified Infrastructure.Requester as Requester
 
 import Control.Concurrent.Async ( concurrently )
 import Control.Concurrent.MVar  ( newMVar )
-import Data.Aeson               ( (.:) )
+import GHC.Generics             ( Generic )
 
-import qualified Data.Aeson as Aeson
+import qualified Data.Aeson.Extended as Aeson
 
 data Config = Config
   { cLogger   :: Logger.Config
   , cVk       :: Vk.Config
   , cTelegram :: Telegram.Config
-  }
+  } deriving (Generic)
 
 instance Aeson.FromJSON Config where
-  parseJSON = Aeson.withObject "EchoBot.Config" $ \o -> Config
-    <$> o .: "logger"
-    <*> o .: "vk"
-    <*> o .: "telegram"
+  parseJSON = Aeson.parseJsonDrop
 
 run :: IO ()
 run = do
-  lock <- newMVar ()
   config <- Aeson.eitherDecodeFileStrict "bot.conf.local"
   case (config :: Either String Config) of
     Left l -> putStrLn l
     Right Config {..} -> do
+      lock    <- newMVar ()
       let vk  = Vk.mkApp       cVk       cLogger lock
           tel = Telegram.mkApp cTelegram cLogger lock
       concurrently (Vk.runApp vk) (Telegram.runApp tel)
