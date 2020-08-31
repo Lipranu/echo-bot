9527fe105f0c7e8e0d3fe5d3cf1c8a8d476fd699
Igor Belousov <lipranu@gmail.com>
2020-08-31 22:38:25 +0700

    Added getLongPollServer request

--- src/App/Vk.hs
+++ src/App/Vk.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses  #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE TypeApplications           #-}
 
 module App.Vk ( Config, mkApp, runApp ) where
 
@@ -15,11 +16,13 @@ import qualified Infrastructure.Logger    as Logger
 import qualified Infrastructure.Requester as Requester
 
 import Control.Monad.IO.Class ( MonadIO, liftIO )
-import Control.Monad.Reader   ( ReaderT, MonadReader, runReaderT )
+import Control.Monad.Reader   ( ReaderT, MonadReader, runReaderT, asks )
 import Control.Exception      ( try )
 import Data.Aeson             ( (.:) )
 import Data.Text              ( Text )
+import Data.Text.Encoding     ( encodeUtf8 )
 import Data.Time              ( getCurrentTime )
+import GHC.Generics           ( Generic )
 
 import qualified Data.Aeson.Extended as Aeson
 import qualified Data.Text           as Text
@@ -57,6 +60,12 @@ instance Has (Logger App) Env where
 instance Has (Requester App) Env where
   getter = envRequester
 
+instance Has Token Env where
+  getter = envToken
+
+instance Has Group Env where
+  getter = envGroup
+
 newtype App a = App { unApp :: ReaderT Env IO a } deriving
   (Functor, Applicative, Monad, MonadReader Env, MonadIO)
 
@@ -72,10 +81,14 @@ instance Requester.MonadRequester App where
 
 app :: App ()
 app = do
-  Logger.logDebug ("Debug Message" :: Text)
-  Logger.logInfo ("Info Message" :: Text)
-  Logger.logWarning ("Warning Message" :: Text)
-  Logger.logError ("Error Message" :: Text)
+  r <- getLongPollServer >>= Requester.requestAndDecode @GetServer
+  case r of
+    Requester.Result v -> Logger.logDebug $ show v
+    Requester.DecodeError e v -> Logger.logError e >> Logger.logDebug v
+    Requester.RequestError e -> Logger.logError e
+--  Logger.logInfo ("Info Message" :: Text)
+--  Logger.logWarning ("Warning Message" :: Text)
+--  Logger.logError ("Error Message" :: Text)
 
 mkApp :: Config -> Logger.Config -> Lock -> HTTP.Manager -> Env
 mkApp Config {..} cLogger lock = Env cToken cGroup logger lock
@@ -84,3 +97,31 @@ mkApp Config {..} cLogger lock = Env cToken cGroup logger lock
 
 runApp :: Env -> IO ()
 runApp = runReaderT (unApp app)
+
+getLongPollServer :: App GetLongPollServer
+getLongPollServer = GetLongPollServer
+  <$> asks getter
+  <*> asks getter
+
+data GetLongPollServer = GetLongPollServer Token Group
+
+instance Requester.ToRequest GetLongPollServer where
+  toRequest (GetLongPollServer (Token t) (Group g))
+    = mkBody $ defaultRequest { HTTP.path = "/method/groups.getLongPollServer" }
+    where mkBody = HTTP.urlEncodedBody
+                     [ ("access_token", encodeUtf8 t)
+                     , ("group_id", encodeUtf8 g)
+                     , ("v", "5.122")
+                     ]
+
+data GetServer = GetServer
+  { guKey :: Text
+  , guTs  :: Text
+  , guServer :: Text
+  } deriving (Generic, Show)
+
+instance Aeson.FromJSON GetServer where
+  parseJSON = Aeson.parseJsonDrop
+
+defaultRequest :: HTTP.Request
+defaultRequest = HTTP.defaultRequest { HTTP.host = "api.vk.com" }
