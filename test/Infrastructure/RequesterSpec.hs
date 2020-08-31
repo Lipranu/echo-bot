eaf77f0432eb292033f793bd23ad88feedc816b8
Igor Belousov <lipranu@gmail.com>
2020-08-31 15:53:00 +0700

    Added types and instances for testing

--- test/Infrastructure/RequesterSpec.hs
+++ test/Infrastructure/RequesterSpec.hs
@@ -1,16 +1,36 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
+
 module Infrastructure.RequesterSpec ( spec ) where
 
 -- IMPORTS ---------------------------------------------------------------------
 
+import Internal
 import Infrastructure.Requester
 
-import Test.Hspec               ( Spec, context, describe, it, shouldBe )
-import Test.Hspec.QuickCheck    ( prop )
-import Test.QuickCheck          ( Arbitrary (..), elements )
+import Control.Monad.Reader         ( Reader, asks )
+import Network.HTTP.Client.Extended ( HttpException, Response )
+import Test.Hspec                   ( Spec, context, describe, it, shouldBe )
+import Test.Hspec.QuickCheck        ( prop )
+import Test.QuickCheck              ( Arbitrary (..), elements )
+
+import qualified Data.ByteString.Lazy as BSL
 
 -- TYPES AND INSTANCES ---------------------------------------------------------
 
+type Test = Reader Env
 
+data Env = Env
+  { envRequester :: Requester Test
+  , envResult    :: Either HttpException (Response BSL.ByteString)
+  }
+
+instance MonadRequester Test where
+  requester _ _ = asks envResult
+
+instance Has (Requester Test) Env where
+  getter = envRequester
 
 -- FUNCTIONS -------------------------------------------------------------------
 
