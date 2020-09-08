91f372bf5f67464b3df02434529e43166a396b87
Igor Belousov <lipranu@gmail.com>
2020-09-08 12:31:11 +0700

    All responses types have been moved to a separate module

--- src/App/Vk.hs
+++ src/App/Vk.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE DeriveGeneric              #-}
-{-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
@@ -12,6 +10,7 @@ module App.Vk ( Config, mkApp, runApp ) where
 
 import App.Vk.Internal
 import App.Vk.Requests
+import App.Vk.Responses
 
 import Infrastructure.Logger    hiding ( Config, Priority (..) )
 import Infrastructure.Requester
@@ -19,19 +18,15 @@ import Internal
 
 import qualified Infrastructure.Logger as Logger
 
-import Control.Applicative         ( (<|>) )
 import Control.Exception           ( try )
 import Control.Monad.IO.Class      ( MonadIO, liftIO )
 import Control.Monad.Reader        ( ReaderT, MonadReader, runReaderT, lift )
 import Control.Monad.State         ( StateT, execStateT, modify, gets )
-import Data.Aeson                  ( (.:), (.:?) )
+import Data.Aeson                  ( (.:) )
 import Data.Foldable               ( traverse_ )
-import Data.Maybe                  ( fromMaybe )
 import Data.Text.Encoding.Extended ( decodeUtf8 )
 import Data.Text.Extended          ( Text )
 import Data.Time                   ( getCurrentTime )
-import Data.Typeable               ( Typeable, typeOf )
-import GHC.Generics                ( Generic )
 import System.Random               ( randomIO )
 
 import qualified Data.Aeson.Extended          as Aeson
@@ -41,8 +36,6 @@ import qualified Data.Text.IO                 as TextIO
 import qualified Network.HTTP.Client.Extended as HTTP
 
 -- TYPES AND INSTANCES -----------------------------------------------------
-
--- Config and Env ----------------------------------------------------------
 
 data Config = Config
   { cToken :: Token
@@ -68,8 +61,6 @@ instance Has (Requester App) Env where getter = envRequester
 instance Has Token           Env where getter = envToken
 instance Has Group           Env where getter = envGroup
 
--- App ---------------------------------------------------------------------
-
 newtype App a = App { unApp :: ReaderT Env IO a } deriving
   (Functor, Applicative, Monad, MonadReader Env, MonadIO, MonadFail )
 
@@ -82,219 +73,6 @@ instance MonadTime App where
 
 instance MonadRequester App where
   requester manager req = liftIO $ try $ HTTP.httpLbs req manager
-
--- Response ----------------------------------------------------------------
-
-data Response a
-  = Success a
-  | Error ErrorResponse
-
-instance Aeson.FromJSON a => Aeson.FromJSON (Response a) where
-  parseJSON = Aeson.withObject "App.Vk.Response" $ \o ->
-        Success <$> o .: "response"
-    <|> Error   <$> o .: "error"
-
-instance Typeable a => Loggable (Response a) where
-  toLog (Success x) =
-    "Successfully received response of type: " <> Text.showt (typeOf x)
-
-  toLog (Error   x) = toLog x
-
--- ErrorResponse -----------------------------------------------------------
-
-data ErrorResponse = ErrorResponse
-  { eErrorCode     :: Integer
-  , eErrorMsg      :: Text
-  , eRequestParams :: [RequestParams]
-  } deriving Generic
-
-instance Aeson.FromJSON ErrorResponse where
-  parseJSON = Aeson.parseJsonDrop
-
-instance Loggable ErrorResponse where
-  toLog ErrorResponse {..}
-    = "An error occurred as a result of the request\n\
-    \ | Error Code: "        <> Text.showt eErrorCode <> "\n\
-    \ | Error Message: "     <> eErrorMsg             <> "\n\
-    \ | Request Parameters:" <> params
-    where params  = foldr (<>) "" $ fmap toLog eRequestParams
-
--- RequestParams -----------------------------------------------------------
-
-data RequestParams = RequestParams
-  { rpKey   :: Text
-  , rpValue :: Text
-  } deriving Generic
-
-instance Aeson.FromJSON RequestParams where
-  parseJSON = Aeson.parseJsonDrop
-
-instance Loggable RequestParams where
-  toLog RequestParams {..} = "\n | \t" <> rpKey <> ": " <> rpValue
-
--- Updates -----------------------------------------------------------------
-
-data Updates
-  = Updates [Aeson.Value] Text
-  | OutOfDate Text
-  | KeyExpired
-  | DataLost
-
-instance Aeson.FromJSON Updates where
-  parseJSON = Aeson.withObject "App.Vk.Updates" $ \o -> do
-    result <- o .:? "updates"
-    case result of
-      Just updates -> do
-        ts <- o .: "ts"
-        return $ Updates updates ts
-      Nothing -> do
-        code <- o .: "failed"
-        case code :: Integer of
-          1 -> OutOfDate <$> o .: "ts"
-          2 -> return KeyExpired
-          3 -> return DataLost
-          e -> fail $ "App.Vk.Updates: Unknown error key: " <> show e
-
-instance Loggable Updates where
-  toLog (Updates upds ts) = "Resived updates:\n\
-    \ | Amount: " <> (Text.showt . length) upds <> "\n\
-    \ | New timestamp: " <> ts
-
-  toLog (OutOfDate ts) =
-    "Event history is outdated or partially lost. \
-    \Performing new request for updates with timestamp: " <> ts
-
-  toLog KeyExpired = "Key expired. Performing request for new key"
-
-  toLog DataLost   = "Information lost. Performing request for new key"
-
--- Update ------------------------------------------------------------------
-
-data Update
-  = NewMessage Message
-  | NotSupported Text
-
-instance Aeson.FromJSON Update where
-  parseJSON = Aeson.withObject "App.Vk.Update" $ \o -> do
-    t <- o .: "type"
-    case t of
-      "message_new" -> NewMessage <$> (o .: "object" >>= (.: "message"))
-      _ -> return $ NotSupported t
-
-instance Loggable Update where
-  toLog (NewMessage m) = toLog m
-  toLog (NotSupported t) = "Not supprted update of type: " <> t
-
--- Message -----------------------------------------------------------------
-
-data Message = Message
-  { mFromId      :: Integer
-  , mPeerId      :: Integer
-  , mText        :: Maybe Text
-  , mLatitude    :: Maybe Double
-  , mLongitude   :: Maybe Double
-  , mAttachments :: [Aeson.Value]
---  , mKeyboard    :: Maybe Aeson.Object
-  }
-
-instance Aeson.FromJSON Message where
-  parseJSON = Aeson.withObject "App.Vk.Message" $ \o -> Message
-    <$> o .:  "from_id"
-    <*> o .:  "peer_id"
-    <*> o .:? "text"
-    <*> (coord o "latitude"  <|> pure Nothing)
-    <*> (coord o "longitude" <|> pure Nothing)
-    <*> o .: "attachments"
-    where coord o t = o .: "geo" >>= (.: "coordinates") >>= (.: t)
-
-instance Loggable Message where
-  toLog Message {..} = "New message recived:\n\
-    \ | from_id: "     <> Text.showt mFromId      <> "\n\
-    \ | peer_id: "     <> Text.showt mPeerId      <> "\n\
-    \ | text: "        <> fromMaybe "" mText               --    <> "\n\
---    \ | attachments: " <> Text.showt mAttachments <> "\n\
---    \ | geo: "         <> Text.showt mGeo         <> "\n\
---    \ | keyboard: "    <> Text.showt mKeyboard
-
--- Attachment --------------------------------------------------------------
-
-data Attachment
-  = Attachment Text AttachmentBody
-  | Document DocumentBody
-
-instance Aeson.FromJSON Attachment where
-  parseJSON = Aeson.withObject "App.Vk.Attachment" $ \o -> do
-    aType      <- o .: "type"
-    case aType of
-      "doc" -> Document         <$> (o .: aType >>= Aeson.parseJSON)
-      _     -> Attachment aType <$> (o .: aType >>= Aeson.parseJSON)
-
-instance Loggable Attachment where
-  toLog _ = "PlaceHolder"--"Proccessing attachment:\n\
---    \ | Type: "     <> aType               <> "\n\
---    \ | Media Id: " <> Text.showt aMediaId <> "\n\
---    \ | Owner Id: " <> Text.showt aOwnerId <> key
---    where key = case aAccessKey of
---            Just v  -> "\n | Access Key: " <> v
---            Nothing -> mempty
-
--- AttachmentBody ----------------------------------------------------------
-
-data AttachmentBody = AttachmentBody
-  { aId        :: Integer
-  , aOwnerId   :: Integer
-  , aAccessKey :: Maybe Text
-  } deriving Generic
-
-instance Aeson.FromJSON AttachmentBody where
-  parseJSON = Aeson.parseJsonDrop
-
--- AttachmentBody ----------------------------------------------------------
-
-data DocumentBody = DocumentBody
-  { dUrl    :: Text
-  , dTitle  :: Text
-  } deriving Generic
-
-instance Aeson.FromJSON DocumentBody where
-  parseJSON = Aeson.parseJsonDrop
-
--- UploadServer ------------------------------------------------------------
-
-newtype UploadServer = UploadServer Text
-
-instance Aeson.FromJSON UploadServer where
-  parseJSON = Aeson.withObject "App.Vk.UploadServer" $ \o ->
-    UploadServer <$> o .: "upload_url"
-
--- FileUploaded ------------------------------------------------------------
-
-newtype FileUploaded = FileUploaded Text
-
-instance Aeson.FromJSON FileUploaded where
-  parseJSON = Aeson.withObject "App.Vk.FileUploaded" $ \o ->
-    FileUploaded <$> o .: "file"
-
-instance Loggable FileUploaded where
-  toLog _ = "Uploaded file placeholder"
-
--- FileSaved ---------------------------------------------------------------
-
-data FileSaved = FileSaved
-  { fsType :: Text
-  , fsMediaId :: Integer
-  , fsOwnerId :: Integer
-  }
-
-instance Aeson.FromJSON FileSaved where
-  parseJSON = Aeson.withObject "App.Vk.FileSaved" $ \o -> do
-    fsType    <- o .: "type"
-    fsMediaId <- o .: fsType >>= (.: "id")
-    fsOwnerId <- o .: fsType >>= (.: "owner_id")
-    return FileSaved {..}
-
-instance Loggable FileSaved where
-  toLog _ = "FileSaved placeholder"
 
 -- FUNCTIONS ---------------------------------------------------------------
 
---
+++ src/App/Vk/Responses.hs
@@ -0,0 +1,245 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
+module App.Vk.Responses
+  ( Attachment (..)
+  , AttachmentBody (..)
+  , DocumentBody (..)
+  , FileSaved (..)
+  , FileUploaded (..)
+  , Message (..)
+  , Response (..)
+  , Update (..)
+  , Updates (..)
+  , UploadServer (..)
+  ) where
+
+-- IMPORTS -----------------------------------------------------------------
+
+import Infrastructure.Logger ( Loggable (..) )
+
+import Control.Applicative ( (<|>) )
+import Data.Aeson          ( (.:), (.:?) )
+import Data.Maybe          ( fromMaybe )
+import Data.Text.Extended  ( Text )
+import Data.Typeable       ( Typeable, typeOf )
+import GHC.Generics        ( Generic )
+
+import qualified Data.Aeson.Extended as Aeson
+import qualified Data.Text.Extended  as Text
+
+-- TYPES AND INSTANCES -----------------------------------------------------
+
+-- Response ----------------------------------------------------------------
+
+data Response a
+  = Success a
+  | Error ErrorResponse
+
+instance Aeson.FromJSON a => Aeson.FromJSON (Response a) where
+  parseJSON = Aeson.withObject "App.Vk.Response" $ \o ->
+        Success <$> o .: "response"
+    <|> Error   <$> o .: "error"
+
+instance Typeable a => Loggable (Response a) where
+  toLog (Success x) =
+    "Successfully received response of type: " <> Text.showt (typeOf x)
+
+  toLog (Error   x) = toLog x
+
+-- ErrorResponse -----------------------------------------------------------
+
+data ErrorResponse = ErrorResponse
+  { eErrorCode     :: Integer
+  , eErrorMsg      :: Text
+  , eRequestParams :: [RequestParams]
+  } deriving Generic
+
+instance Aeson.FromJSON ErrorResponse where
+  parseJSON = Aeson.parseJsonDrop
+
+instance Loggable ErrorResponse where
+  toLog ErrorResponse {..}
+    = "An error occurred as a result of the request\n\
+    \ | Error Code: "        <> Text.showt eErrorCode <> "\n\
+    \ | Error Message: "     <> eErrorMsg             <> "\n\
+    \ | Request Parameters:" <> params
+    where params  = foldr (<>) "" $ fmap toLog eRequestParams
+
+-- RequestParams -----------------------------------------------------------
+
+data RequestParams = RequestParams
+  { rpKey   :: Text
+  , rpValue :: Text
+  } deriving Generic
+
+instance Aeson.FromJSON RequestParams where
+  parseJSON = Aeson.parseJsonDrop
+
+instance Loggable RequestParams where
+  toLog RequestParams {..} = "\n | \t" <> rpKey <> ": " <> rpValue
+
+-- Updates -----------------------------------------------------------------
+
+data Updates
+  = Updates [Aeson.Value] Text
+  | OutOfDate Text
+  | KeyExpired
+  | DataLost
+
+instance Aeson.FromJSON Updates where
+  parseJSON = Aeson.withObject "App.Vk.Updates" $ \o -> do
+    result <- o .:? "updates"
+    case result of
+      Just updates -> do
+        ts <- o .: "ts"
+        return $ Updates updates ts
+      Nothing -> do
+        code <- o .: "failed"
+        case code :: Integer of
+          1 -> OutOfDate <$> o .: "ts"
+          2 -> return KeyExpired
+          3 -> return DataLost
+          e -> fail $ "App.Vk.Updates: Unknown error key: " <> show e
+
+instance Loggable Updates where
+  toLog (Updates upds ts) = "Resived updates:\n\
+    \ | Amount: " <> (Text.showt . length) upds <> "\n\
+    \ | New timestamp: " <> ts
+
+  toLog (OutOfDate ts) =
+    "Event history is outdated or partially lost. \
+    \Performing new request for updates with timestamp: " <> ts
+
+  toLog KeyExpired = "Key expired. Performing request for new key"
+
+  toLog DataLost   = "Information lost. Performing request for new key"
+
+-- Update ------------------------------------------------------------------
+
+data Update
+  = NewMessage Message
+  | NotSupported Text
+
+instance Aeson.FromJSON Update where
+  parseJSON = Aeson.withObject "App.Vk.Update" $ \o -> do
+    t <- o .: "type"
+    case t of
+      "message_new" -> NewMessage <$> (o .: "object" >>= (.: "message"))
+      _ -> return $ NotSupported t
+
+instance Loggable Update where
+  toLog (NewMessage m) = toLog m
+  toLog (NotSupported t) = "Not supprted update of type: " <> t
+
+-- Message -----------------------------------------------------------------
+
+data Message = Message
+  { mFromId      :: Integer
+  , mPeerId      :: Integer
+  , mText        :: Maybe Text
+  , mLatitude    :: Maybe Double
+  , mLongitude   :: Maybe Double
+  , mAttachments :: [Aeson.Value]
+--  , mKeyboard    :: Maybe Aeson.Object
+  }
+
+instance Aeson.FromJSON Message where
+  parseJSON = Aeson.withObject "App.Vk.Message" $ \o -> Message
+    <$> o .:  "from_id"
+    <*> o .:  "peer_id"
+    <*> o .:? "text"
+    <*> (coord o "latitude"  <|> pure Nothing)
+    <*> (coord o "longitude" <|> pure Nothing)
+    <*> o .: "attachments"
+    where coord o t = o .: "geo" >>= (.: "coordinates") >>= (.: t)
+
+instance Loggable Message where
+  toLog Message {..} = "New message recived:\n\
+    \ | from_id: "     <> Text.showt mFromId      <> "\n\
+    \ | peer_id: "     <> Text.showt mPeerId      <> "\n\
+    \ | text: "        <> fromMaybe "" mText               --    <> "\n\
+--    \ | attachments: " <> Text.showt mAttachments <> "\n\
+--    \ | geo: "         <> Text.showt mGeo         <> "\n\
+--    \ | keyboard: "    <> Text.showt mKeyboard
+
+-- Attachment --------------------------------------------------------------
+
+data Attachment
+  = Attachment Text AttachmentBody
+  | Document DocumentBody
+
+instance Aeson.FromJSON Attachment where
+  parseJSON = Aeson.withObject "App.Vk.Attachment" $ \o -> do
+    aType      <- o .: "type"
+    case aType of
+      "doc" -> Document         <$> (o .: aType >>= Aeson.parseJSON)
+      _     -> Attachment aType <$> (o .: aType >>= Aeson.parseJSON)
+
+instance Loggable Attachment where
+  toLog _ = "PlaceHolder"--"Proccessing attachment:\n\
+--    \ | Type: "     <> aType               <> "\n\
+--    \ | Media Id: " <> Text.showt aMediaId <> "\n\
+--    \ | Owner Id: " <> Text.showt aOwnerId <> key
+--    where key = case aAccessKey of
+--            Just v  -> "\n | Access Key: " <> v
+--            Nothing -> mempty
+
+-- AttachmentBody ----------------------------------------------------------
+
+data AttachmentBody = AttachmentBody
+  { aId        :: Integer
+  , aOwnerId   :: Integer
+  , aAccessKey :: Maybe Text
+  } deriving Generic
+
+instance Aeson.FromJSON AttachmentBody where
+  parseJSON = Aeson.parseJsonDrop
+
+-- AttachmentBody ----------------------------------------------------------
+
+data DocumentBody = DocumentBody
+  { dUrl    :: Text
+  , dTitle  :: Text
+  } deriving Generic
+
+instance Aeson.FromJSON DocumentBody where
+  parseJSON = Aeson.parseJsonDrop
+
+-- UploadServer ------------------------------------------------------------
+
+newtype UploadServer = UploadServer Text
+
+instance Aeson.FromJSON UploadServer where
+  parseJSON = Aeson.withObject "App.Vk.UploadServer" $ \o ->
+    UploadServer <$> o .: "upload_url"
+
+-- FileUploaded ------------------------------------------------------------
+
+newtype FileUploaded = FileUploaded Text
+
+instance Aeson.FromJSON FileUploaded where
+  parseJSON = Aeson.withObject "App.Vk.FileUploaded" $ \o ->
+    FileUploaded <$> o .: "file"
+
+instance Loggable FileUploaded where
+  toLog _ = "Uploaded file placeholder"
+
+-- FileSaved ---------------------------------------------------------------
+
+data FileSaved = FileSaved
+  { fsType :: Text
+  , fsMediaId :: Integer
+  , fsOwnerId :: Integer
+  }
+
+instance Aeson.FromJSON FileSaved where
+  parseJSON = Aeson.withObject "App.Vk.FileSaved" $ \o -> do
+    fsType    <- o .: "type"
+    fsMediaId <- o .: fsType >>= (.: "id")
+    fsOwnerId <- o .: fsType >>= (.: "owner_id")
+    return FileSaved {..}
+
+instance Loggable FileSaved where
+  toLog _ = "FileSaved placeholder"
