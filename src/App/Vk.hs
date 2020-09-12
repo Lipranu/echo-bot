126f8b33cc1e8037ab8b85b5ab76f9b1035aba80
Igor Belousov <lipranu@gmail.com>
2020-09-12 09:51:51 +0700

    Refactored rest of vk app routes

--- src/App/Vk.hs
+++ src/App/Vk.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE TypeApplications           #-}
 
 module App.Vk ( Config, mkApp, runApp ) where
 
@@ -21,8 +22,7 @@ import qualified Infrastructure.Logger as Logger
 
 import Control.Exception           ( try )
 import Control.Monad.IO.Class      ( MonadIO, liftIO )
-import Control.Monad.Reader        ( ReaderT (..), MonadReader
-                                   , runReaderT, lift )
+import Control.Monad.Reader        ( ReaderT (..), MonadReader, runReaderT )
 import Control.Monad.State         ( StateT (..), execStateT )
 import Data.Aeson                  ( (.:) )
 import Data.Foldable               ( traverse_ )
@@ -99,12 +99,11 @@ runApp :: Env -> IO ()
 runApp = runReaderT (unApp app)
 
 getLongPollServer :: App ()
-getLongPollServer = handleErrorRequest
-  GetLongPollServer
-  (getUpdates . mkGetUpdates)
+getLongPollServer = handleErrorRequest GetLongPollServer
+  $ getUpdates . mkGetUpdates
 
 getUpdates :: GetUpdates -> App ()
-getUpdates gu = handleErrorRequest gu (routeUpdates gu)
+getUpdates gu = handleErrorRequest gu $ routeUpdates gu
 
 routeUpdates :: GetUpdates -> Updates -> App ()
 routeUpdates gu (Updates upd ts) = do
@@ -119,7 +118,7 @@ processUpdate err = logWarning err
 
 processNewMessage :: Message -> App ()
 processNewMessage message = do
-  aState   <- execStateT
+  aState      <- execStateT
     (traverseHandle routeAttachment $ parse <$> mAttachments message) $
     mkState message
   sendMessage <- mkSendMessage message aState <$> liftIO randomIO
@@ -137,43 +136,20 @@ routeAttachment (Sticker      id) = addSticker id
 
 processDocument :: DocumentBody -> StateT AttachmentsState App ()
 processDocument doc = do
-  (gFile, gUploadServer) <- mkUploadRequests doc
-  lift $ logInfo gFile
-  file <- lift $ request gFile
-  lift $ logInfo gUploadServer
-  uploadServer <- lift $ requestAndDecode gUploadServer
-  handle file uploadServer
-  where handle :: Result LBS.ByteString
-               -> Result (Response UploadServer)
-               -> StateT AttachmentsState App ()
-        handle (Result file) (Result (Success us)) = do
-          lift $ logDebug us
-          uploadDocument $ mkUploadFile us file doc
-        handle (Result _) error = lift $ logWarning error
-        handle (RequestError error) (Result (Success _)) =
-          lift $ logWarning error
-        handle (RequestError error1) error2 = do
-          lift $ logWarning error1
-          lift $ logWarning error2
+  gUploadServer <- mkGetUploadServer
+  handleWarningRequest gUploadServer $ getFile doc
+
+getFile :: DocumentBody -> UploadServer -> StateT AttachmentsState App ()
+getFile doc us = do
+  file <- request $ mkGetFile doc
+  handleWarningR (uploadDocument . mkUploadFile doc us) file
 
 uploadDocument :: UploadFile -> StateT AttachmentsState App ()
-uploadDocument uFile = do
-  lift $ logInfo uFile
-  lift (requestAndDecode uFile) >>= handle
-  where handle :: Result FileUploaded -> StateT AttachmentsState App ()
-        handle (Result result@(FileUploaded file)) = do
-          lift $ logDebug result
-          saveFile $ mkSaveFile uFile file
-        handle error = lift $ logWarning error
+uploadDocument uFile = handleWarningRequest uFile
+  $ saveFile . mkSaveFile uFile
 
 saveFile :: SaveFile -> StateT AttachmentsState App ()
-saveFile sf = do
-  lift $ logInfo sf
-  lift (requestAndDecode sf) >>= handle
-  where handle :: Result (Response FileSaved)
-               -> StateT AttachmentsState App ()
-        handle (Result (Success x)) = lift (logDebug x) >> addAttachment x
-        handle error = lift $ logWarning error
+saveFile sf = handleWarningRequest @FileSaved sf addAttachment
 
 -- TODO: move all handlers to shared module
 handle :: ( HasLogger r m, Loggable a)
@@ -217,7 +193,7 @@ requestWithLog ::
     -> m (Result (Response output))
 requestWithLog x = logInfo x >> requestAndDecode x
 
-handleErrorRequest, handleWarningRequest ::
+handleErrorRequest, handleWarningRequest :: forall output input r m .
   ( MonadEffects r m
   , Loggable input
   , Loggable output
@@ -236,6 +212,7 @@ traverseHandle ::
     -> [Result input]
     -> m ()
 traverseHandle f = traverse_ (handleWarningR f)
+
 start, shutdown :: Text
 start    = "Application getting started"
 shutdown = "Application shut down"
