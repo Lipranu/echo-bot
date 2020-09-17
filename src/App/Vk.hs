2910e836f45100d36859b9c794aa5c0a32dfc3a6
Igor Belousov <lipranu@gmail.com>
2020-09-17 20:26:18 +0700

    Added MonadCont instance to App

--- src/App/Vk.hs
+++ src/App/Vk.hs
@@ -23,13 +23,14 @@ import qualified Infrastructure.Logger as Logger
 import Control.Exception      ( try )
 import Control.Monad.IO.Class ( MonadIO, liftIO )
 import Control.Monad.Reader   ( ReaderT (..), MonadReader )
+import Control.Monad.Cont     ( ContT (..), MonadCont )
 import Data.Aeson             ( (.:) )
 import Data.Time              ( getCurrentTime )
 import Data.IORef             ( IORef )
 import Network.HTTP.Client    ( Manager, httpLbs )
 
-import qualified Data.Aeson.Extended  as Aeson
-import qualified Data.Text.IO         as TextIO
+import qualified Data.Aeson.Extended as Aeson
+import qualified Data.Text.IO        as TextIO
 
 -- TYPES AND INSTANCES -----------------------------------------------------
 
@@ -48,40 +49,40 @@ data Env = Env
   , envGroup         :: Group
   , envDefaultRepeat :: DefaultRepeat
   , envRepetitions   :: IORef Repetitions
-  , envLogger        :: Logger App
+  , envLogger        :: Logger (App Env ())
   , envLock          :: Lock
-  , envRequester     :: Requester App
+  , envRequester     :: Requester (App Env ())
   , envHelpText      :: HelpText
   , envRepeatText    :: RepeatText
   }
 
-instance Has Lock                Env where getter = envLock
-instance Has (Logger App)        Env where getter = envLogger
-instance Has (Requester App)     Env where getter = envRequester
-instance Has Token               Env where getter = envToken
-instance Has Group               Env where getter = envGroup
-instance Has DefaultRepeat       Env where getter = envDefaultRepeat
-instance Has HelpText            Env where getter = envHelpText
-instance Has RepeatText          Env where getter = envRepeatText
-instance Has (IORef Repetitions) Env where getter = envRepetitions
+instance Has Lock                  Env where getter = envLock
+instance Has (Logger (App Env ()))    Env where getter = envLogger
+instance Has (Requester (App Env ())) Env where getter = envRequester
+instance Has Token                 Env where getter = envToken
+instance Has Group                 Env where getter = envGroup
+instance Has DefaultRepeat         Env where getter = envDefaultRepeat
+instance Has HelpText              Env where getter = envHelpText
+instance Has RepeatText            Env where getter = envRepeatText
+instance Has (IORef Repetitions)   Env where getter = envRepetitions
 
-newtype App a = App { unApp :: ReaderT Env IO a } deriving
-  (Functor, Applicative, Monad, MonadReader Env, MonadIO, MonadFail )
+newtype App r c a = App { unApp :: ReaderT r (ContT c IO) a } deriving
+  ( Functor, Applicative, Monad, MonadReader r, MonadIO, MonadFail, MonadCont )
 
-instance MonadLogger App where
+instance MonadLogger (App r c) where
   logConsole   = liftIO . TextIO.putStr
   logFile path = liftIO . TextIO.appendFile path
 
-instance MonadTime App where
+instance MonadTime (App r c) where
   getTime = liftIO getCurrentTime
 
-instance MonadRequester App where
+instance MonadRequester (App r c) where
   requester manager req = liftIO $ try $ httpLbs req manager
 
 -- FUNCTIONS ---------------------------------------------------------------
 
-app :: App ()
-app = start >> getLongPollServer
+app :: App Env () ()
+app = start >> getLongPollServer -- >>= endRoute-- >>= getUpdates' >>= endRoute-- >>= getUpdates'  --endRoute
 
 mkApp :: Config
       -> Shared.Config
@@ -103,4 +104,4 @@ mkApp Config {..} Shared.Config {..} logger lock ref manager =
    in Env {..}
 
 runApp :: Env -> IO ()
-runApp = runReaderT (unApp app)
+runApp env = runContT (runReaderT (unApp app) env) return --shutdown--return --endRoute
